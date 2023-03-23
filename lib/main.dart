c9120a36c386649f4982622fb45e26d69e03567c
IsaTunahan <118114545+IsaTunahan@users.noreply.github.com>
2023-03-23 12:47:14 +0300

    Değişkenler Eklendi

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp()); 
+  
+
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+Color _textColor = Colors.green;
+Color _buttonColor = Colors.red;
+Color _backroundColor = Colors.white;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
